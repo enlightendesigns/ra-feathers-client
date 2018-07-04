8b5f264aa6122d2f95b805b2a9909651201b1c01
Alexandre Guidet <alex@room9.co.nz>
2018-07-05 10:26:54 +1200

    completely renamed the main functions

--- src/ra-feathers-client.ts
+++ src/ra-feathers-client.ts
@@ -1,7 +1,7 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-import dataProvider from './providers/feathers-data-provider'
-import authProvider from './providers/feathers-auth-provider'
+import feathersDataProvider from './providers/feathers-data-provider'
+import feathersAuthProvider from './providers/feathers-auth-provider'
 
-export { dataProvider, authProvider }
+export { feathersDataProvider, feathersAuthProvider }
